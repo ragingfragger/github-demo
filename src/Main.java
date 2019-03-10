7b7a3210e5a37bd1c762f6129d81f9b7ca7ecf85
hjones <hjones@expedia.com>
2019-03-10 10:57:30 -0700

    Add java file to test BlueJ

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main class for the demo -- testing BlueJ support.
+ */
+
+public class Main{
+    public static void main(String [] args){
+        System.out.println("Welcome to the demo!");
+    }
+}
