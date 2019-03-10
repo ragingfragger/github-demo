202fa41aff3449d9cb3d05d7561913d1dd4b1b4b
Helen Jones <helenlj159@gmail.com>
2019-03-10 11:26:55 -0700

    Add Java files for BlueJ support and add .gitignore. Rename README.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+package src;
+
+import java.util.*;
+
 /**
  * Main class for the demo -- testing BlueJ support.
  */
@@ -5,5 +9,11 @@
 public class Main{
     public static void main(String [] args){
         System.out.println("Welcome to the demo!");
+
+        System.out.println("Is this the Krusty Krab?");
+
+        Restaurant restaurant = new Restaurant();
+        
+        restaurant.isThisTheKrustyKrab();
     }
 }
---
+++ src/Restaurant.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.*;
+
+
+/**
+ * Write a description of class KrustyKrab here.
+ *
+ * @author hjones
+ * @version 0.0
+ */
+public class Restaurant
+{
+    
+    /**
+     * Constructor for objects of class KrustyKrab
+     */
+    public Restaurant()
+    {
+       
+    }
+
+    /**
+     * A method to determine if this is, in fact, the Krusty Krab
+     *
+     */
+    public void isThisTheKrustyKrab()
+    {
+        System.out.println("NO, this is Patrick");
+    }
+}
